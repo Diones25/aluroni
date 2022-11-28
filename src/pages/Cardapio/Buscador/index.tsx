25a8b1295a01e5160faf7e32aa450f45f1869e2f
Diones <pereiradiones987@gmail.com>
2022-11-27 21:07:20 -0300

    first commit

---
+++ src/pages/Cardapio/Buscador/index.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import styles from './Buscador.module.scss';
+import { CgSearch } from 'react-icons/cg';
+
+interface Props {
+    busca: string,
+    setBusca: React.Dispatch<React.SetStateAction<string>>
+}
+
+export function Buscador({ busca, setBusca }: Props) {
+    
+
+    return (
+        <>
+            <div className={styles.buscador}>
+                <input 
+                    value={busca}
+                    onChange={evento => setBusca(evento.target.value)}
+                    placeholder="Buscar"
+                />
+                <CgSearch size={25} color='#4c4d5f'/>
+            </div>
+        </>
+    )
+}
