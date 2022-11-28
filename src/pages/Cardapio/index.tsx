2c15e4ba7b9fb5c64260b7b2105e90754314651b
Diones <pereiradiones987@gmail.com>
2022-11-27 21:41:47 -0300

    ajustando indentação

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -1,48 +1,36 @@
-import React from 'react';
-import styles from './Cardapio.module.scss';
-import Logo from 'assets/logo.svg';
-import { Buscador } from './Buscador';
-import { useState } from 'react';
-import { Filtros } from './Filtros';
-import { Ordenador } from './Ordenador';
-import { Itens } from './Itens';
+import React from "react";
+import styles from "./Cardapio.module.scss";
+import Logo from "assets/logo.svg";
+import { Buscador } from "./Buscador";
+import { useState } from "react";
+import { Filtros } from "./Filtros";
+import { Ordenador } from "./Ordenador";
+import { Itens } from "./Itens";
 
 export function Cardapio() {
-    const [ busca, setBusca ] = useState('');
-    const [ filtro, setFiltro ] = useState<number | null>(null);
-    const [ ordenador, setOrdenador ] = useState("");
+  const [busca, setBusca] = useState("");
+  const [filtro, setFiltro] = useState<number | null>(null);
+  const [ordenador, setOrdenador] = useState("");
 
-    return (
-        <>
-            <main>
-                <nav className={styles.menu}>
-                    <img src={Logo} alt='Logo do Aluroni' />
-                </nav>
-                <header className={styles.header}>
-                    <div className={styles.header__text}>
-                        A casa do código e da massa
-                    </div>
-                </header>
-                <section className={styles.cardapio}>
-                    <h3 className={styles.cardapio__titulo}>Cardápio</h3>
-                    <Buscador busca={busca} setBusca={setBusca} />
-                    <div className={styles.cardapio__filtros}>
-                        <Filtros
-                            filtro={filtro}
-                            setFiltro={setFiltro}
-                        />
-                        <Ordenador
-                            ordenador={ordenador} 
-                            setOrdenador={setOrdenador}
-                        />
-                    </div>
-                    <Itens 
-                        busca={busca}
-                        filtro={filtro}
-                        ordenador={ordenador}
-                    />
-                </section>
-            </main>      
-        </>
-    )
+  return (
+    <>
+      <main>
+        <nav className={styles.menu}>
+          <img src={Logo} alt="Logo do Aluroni" />
+        </nav>
+        <header className={styles.header}>
+          <div className={styles.header__text}>A casa do código e da massa</div>
+        </header>
+        <section className={styles.cardapio}>
+          <h3 className={styles.cardapio__titulo}>Cardápio</h3>
+          <Buscador busca={busca} setBusca={setBusca} />
+          <div className={styles.cardapio__filtros}>
+            <Filtros filtro={filtro} setFiltro={setFiltro} />
+            <Ordenador ordenador={ordenador} setOrdenador={setOrdenador} />
+          </div>
+          <Itens busca={busca} filtro={filtro} ordenador={ordenador} />
+        </section>
+      </main>
+    </>
+  );
 }
