6d82ebd6130e630d04e28d9535bcae816f5d271b
Diones <pereiradiones987@gmail.com>
2022-12-01 16:56:57 -0300

    finalizando projeto

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,12 +1,37 @@
 import React from 'react';
 import Logo from 'assets/logo.svg';
 import styles from './Menu.module.scss';
+import { Link } from 'react-router-dom';
 
 export default function Menu() {
+  const rotas = [
+    {
+      label: 'Inicio',
+      to: '/'
+    },
+    {
+      label: 'Cardário',
+      to: '/cardapio'
+    },
+    {
+      label: 'Sobre',
+      to: '/sobre'
+    }
+  ]
+
   return (
     <>
       <nav className={styles.menu}>
         <img src={Logo} alt='Logo do Aluroni' />
+        <ul className={styles.menu__list}>
+          {rotas.map((rota, index) => (
+            <li key={index} className={styles.menu__link}>
+              <Link to={rota.to}>
+                {rota.label}
+              </Link>
+            </li>
+          ))}
+        </ul>
       </nav>
     </>
   );
--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
 import styles from './Cardapio.module.scss';
 import { Buscador } from './Buscador';
 import { useState } from 'react';
 import { Filtros } from './Filtros';
 import { Ordenador } from './Ordenador';
 import { Itens } from './Itens';
+import stylesTema from 'styles/Tema.module.scss';
 import Menu from 'components/Menu';
 
 export function Cardapio() {
@@ -14,21 +14,15 @@ export function Cardapio() {
 
   return (
     <>
-      <main> 
-        <Menu />     
-        <header className={styles.header}>
-          <div className={styles.header__text}>A casa do código e da massa</div>
-        </header>
-        <section className={styles.cardapio}>
-          <h3 className={styles.cardapio__titulo}>Cardápio</h3>
-          <Buscador busca={busca} setBusca={setBusca} />
-          <div className={styles.cardapio__filtros}>
-            <Filtros filtro={filtro} setFiltro={setFiltro} />
-            <Ordenador ordenador={ordenador} setOrdenador={setOrdenador} />
-          </div>
-          <Itens busca={busca} filtro={filtro} ordenador={ordenador} />
-        </section>
-      </main>
+      <section className={styles.cardapio}>
+        <h3 className={stylesTema.titulo}>Cardápio</h3>
+        <Buscador busca={busca} setBusca={setBusca} />
+        <div className={styles.cardapio__filtros}>
+          <Filtros filtro={filtro} setFiltro={setFiltro} />
+          <Ordenador ordenador={ordenador} setOrdenador={setOrdenador} />
+        </div>
+        <Itens busca={busca} filtro={filtro} ordenador={ordenador} />
+      </section>
     </>
   );
 }
